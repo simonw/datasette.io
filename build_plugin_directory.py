857d5039e1a2ed429639a4a4af6fcfe4aac7a918
Simon Willison <swillison@gmail.com>
2020-12-09 13:11:47 -0800

    Initial script for populating plugins directory tables
    Refs #20

---
+++ build_plugin_directory.py
@@ -0,0 +1,102 @@
+import click
+from python_graphql_client import GraphqlClient
+import sqlite_utils
+
+
+QUERY = """
+query {
+  search(query:"topic:datasette-io topic:datasette-plugin user:simonw" type:REPOSITORY, first:100) {
+    repositoryCount
+    nodes {
+      ... on Repository {
+        id
+        name
+        nameWithOwner
+        homepageUrl
+        description
+        openGraphImageUrl
+        usesCustomOpenGraphImage
+        stargazerCount
+        forkCount
+        updatedAt
+        createdAt
+        pushedAt
+        repositoryTopics(first:100) {
+          totalCount
+          nodes {
+            topic {
+              name
+            }
+          }
+        }
+        openIssueCount: issues(states:[OPEN]) {
+          totalCount
+        }
+        closedIssueCount: issues(states:[CLOSED]) {
+          totalCount
+        }
+        releases(last: 1) {
+          totalCount
+          nodes {
+            id
+            name
+            tagName
+            description
+            descriptionHTML
+            shortDescriptionHTML
+            createdAt
+            publishedAt
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def transform_node(node):
+    releases = node.pop("releases")
+    node["releaseCount"] = releases["totalCount"]
+    for key in ("openIssueCount", "closedIssueCount"):
+        node[key] = node[key]["totalCount"]
+    repository_topics = node.pop("repositoryTopics")
+    node["topics"] = [n["topic"]["name"] for n in repository_topics["nodes"]]
+    return node, releases["nodes"]
+
+
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+
+def fetch_plugins(oauth_token):
+    data = client.execute(
+        query=QUERY,
+        headers={"Authorization": "Bearer {}".format(oauth_token)},
+    )
+    nodes = data["data"]["search"]["nodes"]
+    return nodes
+
+
+@click.command()
+@click.argument(
+    "db_filename",
+    type=click.Path(file_okay=True, dir_okay=False),
+)
+@click.option("--github-token", envvar="GITHUB_TOKEN", required=True)
+def cli(db_filename, github_token):
+    db = sqlite_utils.Database(db_filename)
+    nodes = fetch_plugins(github_token)
+    for node in nodes:
+        plugin, releases = transform_node(node)
+        db["plugins"].insert(
+            plugin,
+            pk="id",
+            column_order=("id", "name", "description", "homepageUrl", "topics"),
+        )
+        for release in releases:
+            release["plugin"] = plugin["id"]
+            db["releases"].insert(release, pk="id", foreign_keys=("plugin",))
+
+
+if __name__ == "__main__":
+    cli()
