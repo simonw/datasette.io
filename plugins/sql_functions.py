278da3564b6ffc7775c2cdfb048d156df54f9381
Simon Willison <swillison@gmail.com>
2020-12-09 17:46:18 -0800

    Fix for Atom bug, refs #23
    Same bug as https://github.com/simonw/museums/issues/25

--- plugins/sql_functions.py
+++ plugins/sql_functions.py
@@ -2,6 +2,15 @@ from datasette import hookimpl
 from datasette_render_markdown import render_markdown
 
 
+def _render_markdown(markdown):
+    rendered = str(render_markdown(markdown))
+    prefix = '<div style="white-space: normal">'
+    suffix = "</div>"
+    if rendered.startswith(prefix):
+        rendered = rendered[len(prefix) : -len(suffix)]
+    return rendered
+
+
 @hookimpl
 def prepare_connection(conn):
-    conn.create_function("render_markdown", 1, render_markdown)
+    conn.create_function("render_markdown", 1, _render_markdown)
