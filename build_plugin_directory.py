7707160118df2cede2ace5ad0c33123aab5b304a
Simon Willison <swillison@gmail.com>
2020-12-14 12:06:09 -0800

    Fix for incorrect weekly stats query, closes #45

--- build_plugin_directory.py
+++ build_plugin_directory.py
@@ -112,12 +112,19 @@ select
   max(releases.created_at) as latest_release_at,
   plugin_repos.openGraphImageUrl,
   plugin_repos.usesCustomOpenGraphImage,
-  sum(stats.downloads) as downloads_this_week
+  (
+    select
+      sum(downloads)
+    from
+      stats
+    where
+      stats.package = repos.name
+      and stats.date > date('now', '-7 days')
+  ) as downloads_this_week
 from
   plugin_repos
   join repos on plugin_repos.id = repos.node_id
   join releases on repos.id = releases.repo
-  left join stats on repos.name = stats.package and stats.date > date('now', '-7 days')
 group by
   repos.id
 order by
