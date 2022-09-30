475d9352809a38a9d86912578ca1e8c106bde101
Simon Willison <swillison@gmail.com>
2022-09-30 15:23:14 -0700

    datasette.io/discord redirect, closes #119

--- plugins/redirects.py
+++ plugins/redirects.py
@@ -4,8 +4,17 @@ from datasette import hookimpl, Response
 @hookimpl
 def register_routes():
     return (
-        (r"^/conduct/?$", lambda request: Response.redirect(
-            "https://github.com/simonw/datasette/blob/main/CODE_OF_CONDUCT.md",
-            status=301
-        )),
+        (
+            r"^/conduct/?$",
+            lambda request: Response.redirect(
+                "https://github.com/simonw/datasette/blob/main/CODE_OF_CONDUCT.md",
+                status=301,
+            ),
+        ),
+        (
+            r"^/discord/?$",
+            lambda request: Response.redirect(
+                "https://discord.gg/ktd74dm5mw", status=301
+            ),
+        ),
     )
