31ee57c61ff0c39872c96195cb94bca61c3691f3
Simon Willison <swillison@gmail.com>
2023-01-09 09:33:33 -0800

    Link to stable, not latest for extensions help

--- plugins/redirects.py
+++ plugins/redirects.py
@@ -22,7 +22,7 @@ def register_routes():
         (
             r"^/help/extensions/?$",
             lambda request: Response.redirect(
-                "https://docs.datasette.io/en/latest/installation.html#a-note-about-extensions", status=302
+                "https://docs.datasette.io/en/stable/installation.html#a-note-about-extensions", status=302
             ),
         ),   
     )
