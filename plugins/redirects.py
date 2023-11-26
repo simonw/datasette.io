a0ecb3d4459522d228490ed10ee4e8a8786cf9c2
Simon Willison <swillison@gmail.com>
2023-11-26 12:19:34 -0800

    datasette.io/discord-enrichments shortcut

--- plugins/redirects.py
+++ plugins/redirects.py
@@ -23,6 +23,12 @@ def register_routes():
                 "https://discord.gg/RKAH4b8TvE", status=301
             ),
         ),
+        (
+            r"^/discord-enrichments/?$",
+            lambda request: Response.redirect(
+                "https://discord.gg/tcTpMVQdRc", status=301
+            ),
+        ),
         # /help/X may be linked to from the datasette CLI - served with 302 because I may change
         # what they target in the future.
         (
