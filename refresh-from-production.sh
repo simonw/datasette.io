e4ff24285c58f885166c25838e04baba2120884e
Simon Willison <swillison@gmail.com>
2026-05-13 14:23:50 -0700

    Tweak to refresh-from-production.sh

--- refresh-from-production.sh
+++ refresh-from-production.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -euf -o pipefail
 rm -f blog.db
 for f in content.db docs-index.db dogsheep-index.db simon-blog.db tils.db
 do
     rm -f $f
-    wget -q "https://datasette.io/$f"
+    wget -q -O "$f" "https://datasette.io/$f"
     echo $f
 done
